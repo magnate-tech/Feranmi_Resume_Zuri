0fd5d3bac3cfe9868c59b94169b8066c9aacb2c2
magnate-tech <tech.magnatedev@gmail.com>
2021-08-21 11:12:35 -0700

    composer ready and set

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3bcf4ad9673800a29fa7cf6e037b5fc5e8e7fdd4',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3bcf4ad9673800a29fa7cf6e037b5fc5e8e7fdd4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
